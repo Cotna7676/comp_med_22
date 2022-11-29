d6dbd49369f24510d644b59334cf1dde515bcade
Cotna7676 <tnakai7676@gmail.com>
2022-11-29 18:00:46 -0500

    have alexnet and efficientnet working

--- run_models.py
+++ run_models.py
@@ -9,6 +9,7 @@ from torch.utils.data import Dataset, DataLoader
 
 import numpy as np
 from PIL import Image
+from torchmetrics import F1Score
 
 # https://pytorch.org/tutorials/beginner/basics/data_tutorial.html
 
@@ -32,43 +33,13 @@ class CustomImageDataset(Dataset):
         return image, label
 
 from torchvision import transforms, utils
-
-# https://pytorch.org/hub/pytorch_vision_alexnet/
-preprocess = transforms.Compose([
-    transforms.Resize(256),
-    transforms.CenterCrop(224),
-    transforms.RandomRotation(90),
-    transforms.RandomVerticalFlip(),
-    transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-])
-
-train_dataset = CustomImageDataset("train_labels.csv", preprocess)
-val_dataset = CustomImageDataset("val_labels.csv", preprocess)
-test_dataset = CustomImageDataset("test_labels.csv", preprocess)
-
-batch_size = 64
-shuffle = True
-
-train_dataloader = DataLoader(train_dataset, batch_size = batch_size, shuffle = shuffle)
-val_dataloader = DataLoader(val_dataset, batch_size = batch_size, shuffle = False)
-test_dataloader = DataLoader(test_dataset, batch_size = batch_size, shuffle = False)
-
-dataloaders = {'train' : train_dataloader, 'val' : val_dataloader, 'test' : test_dataloader}
-num_train = len(open("train_labels.csv", 'r').readlines())
-num_val = len(open("val_labels.csv", 'r').readlines())
-num_test = len(open("test_labels.csv", 'r').readlines())
-
-print("num train ", num_train, " num val ", num_val)
-
-dataset_sizes = {'train' : num_train, 'val' : num_val, 'test' : num_test}
-
 import copy, time
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+print(device)
 
 # https://pytorch.org/tutorials/beginner/transfer_learning_tutorial.html#load-data
-def train_model(model, criterion, optimizer, num_epochs):
+def train_model(model, criterion, optimizer, num_epochs, dataloaders, dataset_sizes):
     since = time.time()
 
     best_model_wts = copy.deepcopy(model.state_dict())
@@ -91,12 +62,16 @@ def train_model(model, criterion, optimizer, num_epochs):
                 model.eval()   # Set model to evaluate mode
 
             running_loss = 0.0
-            running_corrects = 0
+            # running_corrects = 0
+            f_score_corrects = 0
+
+            num_batches = 0
 
             # Iterate over data.
             for inputs, labels in dataloaders[phase]:
                 inputs = inputs.to(device)
                 labels = labels.to(device)
+                num_batches += 1
 
                 # zero the parameter gradients
                 optimizer.zero_grad()
@@ -114,11 +89,17 @@ def train_model(model, criterion, optimizer, num_epochs):
                         optimizer.step()
 
                 # statistics
+                f1 = F1Score(num_classes = 7)
+                f_score_corrects += f1(preds, labels.data)
+                
                 running_loss += loss.item() * inputs.size(0)
-                running_corrects += torch.sum(preds == labels.data)
+                # running_corrects += torch.sum(preds == labels.data)
 
             epoch_loss = running_loss / dataset_sizes[phase]
-            epoch_acc = running_corrects.double() / dataset_sizes[phase]
+            # epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+            # f score
+            epoch_acc = f_score_corrects.double() / num_batches
 
             if phase == 'train':
                 train_loss[epoch] = epoch_loss
@@ -142,6 +123,7 @@ def train_model(model, criterion, optimizer, num_epochs):
     model.load_state_dict(best_model_wts)
     return model, best_model_wts, train_loss, train_acc, val_loss, val_acc
 
+# save loss accuracy in txt files
 def save_loss_acc(model_name, num_epochs, train_loss, train_acc, val_loss, val_acc):
     f = open(f"{model_name}.log.txt", "w")
     for epoch in range(num_epochs):
@@ -157,12 +139,12 @@ def save_test_loss_acc(model_name, test_loss, test_acc):
     f.write(f"Best Model Test Loss/Acc:{test_loss},{test_acc}")
     f.close()
 
-def run_train_model(model, model_desc, criterion, optimizer, num_epochs):
+# train model
+def run_train_model(model, model_name, criterion, optimizer, num_epochs, dataloaders, dataset_sizes):
     # train model, get train/val acc/loss
     model, best_model_wts, train_loss, train_acc, val_loss, val_acc =\
-                 train_model(model, criterion, optimizer, num_epochs)
+                 train_model(model, criterion, optimizer, num_epochs, dataloaders, dataset_sizes)
 
-    model_name = "artifacts/" + model_desc + "__" + str(num_epochs) + "_epochs"
     save_loss_acc(model_name, num_epochs, train_loss, train_acc, val_loss, val_acc)
 
     best_model = model.load_state_dict(best_model_wts)
@@ -194,29 +176,94 @@ def run_train_model(model, model_desc, criterion, optimizer, num_epochs):
     # save model
     torch.save(model.state_dict(), model_name + ".pth")
 
+# initialize datasets and dataloaders
+def initialize_dataloaders(img_size):
+    # https://pytorch.org/hub/pytorch_vision_alexnet/
+    preprocess = transforms.Compose([
+        transforms.Resize(img_size),
+        transforms.CenterCrop(224),
+        transforms.RandomRotation(90),
+        transforms.RandomVerticalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ])
+
+    train_dataset = CustomImageDataset("train_labels.csv", preprocess)
+    val_dataset = CustomImageDataset("val_labels.csv", preprocess)
+    test_dataset = CustomImageDataset("test_labels.csv", preprocess)
+
+    batch_size = 64
+    shuffle = True
+
+    train_dataloader = DataLoader(train_dataset, batch_size = batch_size, shuffle = shuffle)
+    val_dataloader = DataLoader(val_dataset, batch_size = batch_size, shuffle = False)
+    test_dataloader = DataLoader(test_dataset, batch_size = batch_size, shuffle = False)
+
+    dataloaders = {'train' : train_dataloader, 'val' : val_dataloader, 'test' : test_dataloader}
+    num_train = len(open("train_labels.csv", 'r').readlines())
+    num_val = len(open("val_labels.csv", 'r').readlines())
+    num_test = len(open("test_labels.csv", 'r').readlines())
+
+    print("num train ", num_train, " num val ", num_val)
+
+    dataset_sizes = {'train' : num_train, 'val' : num_val, 'test' : num_test}
+
+    return dataloaders, dataset_sizes
+
 
 # AlexNet -- not modified
 def AlexNet(num_classes, num_epochs):
+    img_size = 256
+    dataloaders, dataset_sizes = initialize_dataloaders(img_size)
+
     model = torchvision.models.alexnet(weights='IMAGENET1K_V1')
-    # print(model)
 
     # freeze layers
     for param in model.parameters():
         param.requires_grad = False
     print(model.parameters)
-
     # have output be number of classes
     model.classifier[6] = nn.Linear(4096,num_classes)
     # print(model)
-    print(model.parameters)
-
+    # print(model.parameters)
     model = model.to(device)
 
     criterion = nn.CrossEntropyLoss()
     # Observe that all parameters are being optimized
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-    run_train_model(model, "AlexNet", criterion, optimizer, num_epochs)
+    model_desc = "AlexNet"
+    model_name = "artifacts/" + model_desc + "__epochs_" + str(num_epochs)
 
+    run_train_model(model, model_name, criterion, optimizer, num_epochs, dataloaders, dataset_sizes)
 
-AlexNet(7, 1)
+# AlexNet -- not modified
+def EfficientNet_b3(num_classes, num_epochs):
+    img_size = 256
+    dataloaders, dataset_sizes = initialize_dataloaders(img_size)
+
+    model = torchvision.models.efficientnet_b3(weights='IMAGENET1K_V1')
+
+    # freeze layers
+    for param in model.parameters():
+        param.requires_grad = False
+    print(model.parameters)
+    # have output be number of classes
+    model.classifier[-1] = nn.Linear(1536,num_classes)
+    # print(model)
+    # print(model.parameters)
+    model = model.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+    # Observe that all parameters are being optimized
+    optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
+
+    model_desc = "EfficientNetB3"
+    model_name = "artifacts/" + model_desc + "__epochs_" + str(num_epochs)
+
+    run_train_model(model, model_name, criterion, optimizer, num_epochs, dataloaders, dataset_sizes)
+
+num_epochs = 100
+num_classes = 7
+# AlexNet(num_classes, num_epochs)
+# EfficientNet_b3(num_classes, num_epochs)
